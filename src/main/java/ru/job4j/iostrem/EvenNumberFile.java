92c860bedfd841abff28c6efacebf39c2e4919b1
EkaterinaSergeevna <ek.belogurova@gmail.com>
2022-07-04 19:30:11 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/iostrem/EvenNumberFile.java
+++ src/main/java/ru/job4j/iostrem/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.iostrem;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -20,7 +21,7 @@ public class EvenNumberFile {
                 }
             }
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
