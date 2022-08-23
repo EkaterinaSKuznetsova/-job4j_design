166580fc3fd445d73049626d2102f04d1dfea1a6
EkaterinaSergeevna <ek.belogurova@gmail.com>
2022-08-23 08:31:25 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/iostrem/Analizy.java
+++ src/main/java/ru/job4j/iostrem/Analizy.java
@@ -3,28 +3,20 @@ import java.io.*;
 
 public class Analizy {
     public static void unavailable(String source, String target) {
-        boolean noWorkServer = false;
         String begin = "";
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-                for (String line = in.readLine(); line != null; line = in.readLine()) {
-                    String[] lines = line.split(" ");
-                    if ((lines[0].equals("400") || lines[0].equals("500")) && !noWorkServer) {
-                        noWorkServer = true;
-                        if (begin.isEmpty()) {
-                           begin = lines[1];
-                        }
-                    }
-                    if ((lines[0].equals("200") || lines[0].equals("300")) && noWorkServer) {
-                        noWorkServer = false;
-                        if (!begin.isEmpty()) {
-                            out.println(begin + ";" + lines[1]);
-                            begin = "";
-                        }
-                    }
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target));
+             BufferedReader in = new BufferedReader(new FileReader(source))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] lines = line.split(" ");
+                boolean noWork = "400".equals(lines[0]) || "500".equals(lines[0]);
+                boolean work = "200".equals(lines[0]) || "300".equals(lines[0]);
+                if (noWork && "".equals(begin)) {
+                    begin = lines[1];
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                if (work && !"".equals(begin)) {
+                    out.println(begin + ";" + lines[1]);
+                    begin = "";
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
